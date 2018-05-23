1193a639dceb1bf61b8afe1617eff7289aaddff8
toipacoelho <toipacoelho@gmail.com>
2018-05-23 22:41:21 +0100

    Version 0.0.6 Participants cant see attributes that mention them as authorized Grand access transaction not writing, needs work

--- test-network/lib/logic.js
+++ test-network/lib/logic.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
  * Track granted authorizations to access attributes
  * @param {pt.ua.attr.GrantAccess} transaction - grant access
@@ -5,7 +7,45 @@
  */
 
  async function GrantAccess(transaction){
+    const me = getCurrentParticipant();
 
+    if(!me){
+        throw new Error('A participant/attribute mapping does not exist');
+    }
+
+    var serviceID = transaction.memberID
+    if (!serviceID){
+        throw new Error('Invalid rquest. \'memberID\' should be defined');
+    }
+
+    var meID = me.getIdentifier();
+
+    console.log ('Mmeber ' + meID + 'grating \'attribute\' access to ' + serviceID);
+
+    return query("getAttributeByOwnerID", {memberID: meID})
+        .then(function (records){
+            if (records.length > 0){
+                var serializer = getSerializer();
+                var attribute = serializer.toJSON(records[0])
+
+                if(!Array.isArray(attribute.authorized)){
+                    attribute.authorized = [];
+                }
+
+                if (attribute.authorized.indexOf(serviceID) < 0) {
+                    degree.authorized.push(serviceID);
+
+                    return getAssetRegistry('pt.ua.attr.Attribute')
+                        .then (function (registry) {registry.update(serializer.fromJSON(attribute))})
+                        .then (function(){
+                            var event = getFactory().newEvent('pt.ua.attr', 'AttrEvent');
+                            event.Attrtransaction = transaction;
+                            emit(event)
+                        });
+                }
+            }
+        })
+        .catch(function (ex) { console.error(ex); throw ex; });
  }
 
  /*
@@ -15,7 +55,45 @@
  */
 
 async function RevokeAccess(transaction){
-     
+    const me = getCurrentParticipant();
+
+    if(!me){
+        throw new Error('A participant/attribute mapping does not exist');
+    }
+
+    var serviceID = transaction.memberID
+    if (!serviceID){
+        throw new Error('Invalid rquest. \'memberID\' should be defined');
+    }
+
+    var meID = me.getIdentifier();
+
+    console.log ('Member ' + meID + 'revokes \'attribute\' access to ' + serviceID);
+
+    return query("getAttributeByOwnerID", {memberID: meID})
+        .then(function (records){
+            if (records.length > 0){
+                var serializer = getSerializer();
+                var attribute = serializer.toJSON(records[0])
+
+                if(!Array.isArray(attribute.authorized)){
+                    var index = attribute.authorized.indexOf(serviceID);
+                    
+                    if (index >= 0) {
+                        degree.authorized.splice(index, 1);
+
+                        return getAssetRegistry('pt.ua.attr.Attribute')
+                            .then (function (registry) {registry.update(serializer.fromJSON(attribute))})
+                            .then (function(){
+                                var event = getFactory().newEvent('pt.ua.attr', 'AttrEvent');
+                                event.Attrtransaction = transaction;
+                                emit(event)
+                            });
+                    }
+                }
+            }
+        })
+        .catch(function (ex) { console.error(ex); throw ex; });
 }
 
 /*
